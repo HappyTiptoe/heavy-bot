58aa249d28cdca588ba135fa2e2b95393ea5c563
ebxn <ebengtuff@gmail.com>
2020-10-17 03:15:14 +0100

    create simple bot that can read .env files

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//
+'use strict'
+
+// load .env variables
+require('dotenv').config()
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('message', (msg) => {
+  if (msg.author === client.user) return
+})
+
+client.on('ready', () => {
+  client.user.setActivity('my weight.', { type: 'WATCHING' })
+  console.log('HEAVY BOT is ready to receive your orders...')
+})
+
+client.login(process.env.SECRET_TOKEN)
