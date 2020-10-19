44bf01f67ecb84808de23d6419321103adb46270
ebxn <ebengtuff@gmail.com>
2020-10-19 16:15:00 +0100

    fix hot command image path

--- src/commandHandlers/handleHot.js
+++ src/commandHandlers/handleHot.js
@@ -1,5 +1,5 @@
 module.exports = (msg) => {
-  msg.channel.send({ files: ['./img/unk.png'] })
+  msg.channel.send({ files: ['dist/img/unk.png'] })
     .then((sentMsg) => {
       sentMsg.react('🇭')
       sentMsg.react('🇴')
