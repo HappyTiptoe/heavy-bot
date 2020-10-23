f786394a7f470ae23796207267c00da6ad3de349
ebxn <ebengtuff@gmail.com>
2020-10-23 01:44:13 +0100

    Add Bill Wibbly reaction

--- src/messageHandlers/handleKeyWords.js
+++ src/messageHandlers/handleKeyWords.js
@@ -1,16 +1,27 @@
 module.exports = (msg) => {
+  const lowerContent = msg.content.toLowerCase()
   // 73
-  if (msg.content.toLowerCase().match(/(?<!\d)73(?!\d)/g)) {
+  if (lowerContent.match(/(?<!\d)73(?!\d)/g)) {
     msg.react('😂')
   }
 
+  // bill wibbly
+  if (
+    lowerContent.includes('bill') ||
+    lowerContent.includes('will') ||
+    lowerContent.includes('wibbly') ||
+    lowerContent.includes('bibbly')
+  ) {
+    msg.react('756948138102882335')
+  }
+
   // heavy
-  if (msg.content.toLowerCase().includes('heavy')) {
+  if (lowerContent.includes('heavy')) {
     msg.react('745365421611417749')
   }
 
   // light
-  if (msg.content.toLowerCase().includes('light')) {
+  if (lowerContent.includes('light')) {
     msg.react('745377252396630176')
   }
 }
