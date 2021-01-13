6f189cfb642a7f88f026c2bf24f0d29570e66cf8
ebxn <eb3ntuff@gmail.com>
2021-01-13 21:47:30 +0000

    add missing ()s

--- src/commandHandlers/handleHelp.js
+++ src/commandHandlers/handleHelp.js
@@ -8,16 +8,16 @@ module.exports = (msg) => {
       {
         name: 'Available Commands:',
         value: `
-          ${getters.getPrefix}help: This command
-          ${getters.getPrefix}choose: Picks between space-separated choices
-          ${getters.getPrefix}decide: Performs a coin flip
-          ${getters.getPrefix}owo: Makes text a li'l more weeby
-          ${getters.getPrefix}popeyes: Generates a random popeyes meal
-          ${getters.getPrefix}prefix: Sets prefix for HEAVY BOT commands
-          ${getters.getPrefix}say: Repeats what you say
-          ${getters.getPrefix}study: Discourages Discord use
-          ${getters.getPrefix}submit egg: Submits an egg
-          ${getters.getPrefix}unkify: i'nm soryr
+          ${getters.getPrefix()}help: This command
+          ${getters.getPrefix()}choose: Picks between space-separated choices
+          ${getters.getPrefix()}decide: Performs a coin flip
+          ${getters.getPrefix()}owo: Makes text a li'l more weeby
+          ${getters.getPrefix()}popeyes: Generates a random popeyes meal
+          ${getters.getPrefix()}prefix: Sets prefix for HEAVY BOT commands
+          ${getters.getPrefix()}say: Repeats what you say
+          ${getters.getPrefix()}study: Discourages Discord use
+          ${getters.getPrefix()}submit egg: Submits an egg
+          ${getters.getPrefix()}unkify: i'nm soryr
         `
       }
     ]
