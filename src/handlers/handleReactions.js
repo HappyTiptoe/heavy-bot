cba4bceaa0fb182efbc81f11163e653f55bd01af
ebxn <eb3ntuff@gmail.com>
2021-02-28 19:09:08 +0000

    remove echo debug console.logs

--- src/handlers/handleReactions.js
+++ src/handlers/handleReactions.js
@@ -1,15 +1,7 @@
 const { echo, goodbye, keywords, weight } = require('../reactions')
 
 const handleReactions = (message, store) => {
-  console.log(
-    'Before Echo',
-    store.getPreviousMessages().map((m) => m.content)
-  )
   echo(message, store)
-  console.log(
-    'After Echo',
-    store.getPreviousMessages().map((m) => m.content)
-  )
   goodbye(message)
   keywords(message)
   weight(message)
